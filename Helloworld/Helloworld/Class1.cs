83409f98f02068ecb6a229bb2c321eb457093f11
Martin Stacey <martin__stacey@hotmail.com>
2019-07-16 22:53:45 -0500

    Add files via upload

---
+++ Helloworld/Helloworld/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.ApplicationServices;
+
+
+
+namespace Helloworld
+{
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Revit", "HelloWorld");
+            return Autodesk.Revit.UI.Result.Succeeded;
+        }
+    }
+}
